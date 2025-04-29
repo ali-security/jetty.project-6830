e9d097c130f0e25dad2627d5415f4c1e8b9c8ddd
Olivier Lamy <olamy@apache.org>
2025-04-29 14:11:47 +1000

    better logging
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- tests/test-distribution/test-distribution-common/src/test/java/org/eclipse/jetty/tests/distribution/DemoModulesTests.java
+++ tests/test-distribution/test-distribution-common/src/test/java/org/eclipse/jetty/tests/distribution/DemoModulesTests.java
@@ -542,7 +542,7 @@ public class DemoModulesTests extends AbstractJettyHomeTest
             try (JettyHomeTester.Run runStart = distribution.start(argsStart))
             {
                 assertTrue(runStart.awaitConsoleLogsFor("Started oejs.Server@", START_TIMEOUT, TimeUnit.SECONDS),
-                        String.join("", runStart.getLogs()));
+                        String.join(System.lineSeparator(), runStart.getLogs()));
 
                 String baseURI = "http://localhost:%d/%s-test".formatted(httpPort, env);
 
@@ -610,7 +610,7 @@ public class DemoModulesTests extends AbstractJettyHomeTest
             try (JettyHomeTester.Run runStart = distribution.start(argsStart))
             {
                 assertTrue(runStart.awaitConsoleLogsFor("Started oejs.Server@", START_TIMEOUT, TimeUnit.SECONDS),
-                    String.join("", runStart.getLogs()));
+                    "LOGS: " + String.join(System.lineSeparator(), runStart.getLogs()));
 
                 String baseURI = "http://localhost:%d/%s-test".formatted(httpPort, env);
 
